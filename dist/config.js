8be5318cbd40784524d2fe55bfe696087f97662a
loehnertz <jakob2102@gmail.com>
2018-09-01 15:52:30 +0200

    Removed `dist` directory again

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const configFile = require('../config.json');
-class Config {
-    static retrieveConfigVariable(key) {
-        return configFile[key];
-    }
-}
-module.exports = Config;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBTSxVQUFVLEdBQUcsT0FBTyxDQUFDLGdCQUFnQixDQUFDLENBQUM7QUFFN0M7SUFDSSxNQUFNLENBQUMsc0JBQXNCLENBQUMsR0FBUTtRQUNsQyxPQUFPLFVBQVUsQ0FBQyxHQUFHLENBQUMsQ0FBQztJQUMzQixDQUFDO0NBQ0o7QUFFRCxNQUFNLENBQUMsT0FBTyxHQUFHLE1BQU0sQ0FBQyJ9
